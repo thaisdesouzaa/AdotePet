7748ddf87d5001d04a635ea26607f5eb8bfb54dc
TheeuzinN <matheuss308@gmail.com>
2021-11-13 12:14:17 -0300

    Criação das Classes pessoa e suas heranças Adotante e doador.

--- AdotePetJavaProjetct/src/Pessoa.java
+++ AdotePetJavaProjetct/src/Pessoa.java
@@ -1,2 +1,57 @@
 public class Pessoa {
+    private char nome;
+    private char cpf;
+    private char dataNascimento;
+    private char endereco;
+    private char email;
+    private char telefone;
+
+
+    public char getNome() {
+        return nome;
+    }
+
+    public void setNome(char nome) {
+        this.nome = nome;
+    }
+
+    public char getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(char cpf) {
+        this.cpf = cpf;
+    }
+
+    public char getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(char dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public char getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(char endereco) {
+        this.endereco = endereco;
+    }
+
+    public char getEmail() {
+        return email;
+    }
+
+    public void setEmail(char email) {
+        this.email = email;
+    }
+
+    public char getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(char telefone) {
+        this.telefone = telefone;
+    }
 }
