7da79d549fe3586e2063fe707f4a8feebd80f7d3
thaisdesouzaa <79648866+thaisdesouzaa@users.noreply.github.com>
2021-11-13 21:19:09 -0300

    Adicionando classe comentario e classe mensagem

---
+++ AdotePetJavaProjetct/src/Comentario.java
@@ -0,0 +1,33 @@
+package javaapplication8;
+
+public class Comentario {
+    
+    private char data;
+    private char hora;
+    private char descricao;
+
+    public char getData() {
+        return data;
+    }
+
+    public void setData(char data) {
+        this.data = data;
+    }
+
+    public char getHora() {
+        return hora;
+    }
+
+    public void setHora(char hora) {
+        this.hora = hora;
+    }
+
+    public char getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(char descricao) {
+        this.descricao = descricao;
+    }
+    
+}
---
+++ AdotePetJavaProjetct/src/Mensagem.java
@@ -0,0 +1,33 @@
+package javaapplication8;
+
+public class Mensagem {
+    
+    private char data;
+    private char hora;
+    private char conteudo;
+
+    public char getData() {
+        return data;
+    }
+
+    public void setData(char data) {
+        this.data = data;
+    }
+
+    public char getHora() {
+        return hora;
+    }
+
+    public void setHora(char hora) {
+        this.hora = hora;
+    }
+
+    public char getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(char conteudo) {
+        this.conteudo = conteudo;
+    }
+       
+}
